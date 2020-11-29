54f8e6c48bc05ddd576b3cb7651f4fe4d17904ef
petross3530mx <petross3530mx@gmail.com>
2020-11-29 22:21:53 +0200

    ts-lint recomended update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,44 +1,44 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-        darkTheme: false,
-        websites: []
-	},
-	mutations: {
-		toggleTheme(state): void {
-            state.darkTheme = !state.darkTheme
-        },
-        addWebsite(state, website){
-            state.websites.push(website)
-        },
-        deleteWebsite(state, website){
-            const stateWebsites = state.websites;
-            const index = stateWebsites.findIndex(w=>w.url == website.url);
-            stateWebsites.splice(index, 1);
-            state.websites = stateWebsites;
-        }
-	},
-	actions: {
-		changeTheme({ commit }) {
-			commit('toggleTheme');
-        },
-        addWebsite({ commit }, website: any){
-            const {url, cms, blockchain, address} = website ;
-            commit('addWebsite', {url, cms, blockchain, address});
-        },
-        deleteWebsite({ commit }, website){
-            commit('deleteWebsite', website);
-        }
-	},
-	getters: {
-        darkTheme: s => s.darkTheme,
-        websites: s => s.websites
+  state: {
+    darkTheme: false,
+    websites: new Array<any>()
+  },
+  mutations: {
+    toggleTheme(state): void {
+      state.darkTheme = !state.darkTheme;
     },
-    plugins: [createPersistedState()],
-	modules: {}
+    addWebsite(state, website: any) {
+      state.websites.push(website as any);
+    },
+    deleteWebsite(state, website) {
+      const stateWebsites: Array<any> = state.websites;
+      const index = stateWebsites.findIndex(w => w.url == website.url);
+      stateWebsites.splice(index, 1);
+      state.websites = stateWebsites as any;
+    }
+  },
+  actions: {
+    changeTheme({ commit }) {
+      commit("toggleTheme");
+    },
+    addWebsite({ commit }, website: any) {
+      const { url, cms, blockchain, address } = website;
+      commit("addWebsite", { url, cms, blockchain, address });
+    },
+    deleteWebsite({ commit }, website) {
+      commit("deleteWebsite", website);
+    }
+  },
+  getters: {
+    darkTheme: s => s.darkTheme,
+    websites: s => s.websites
+  },
+  plugins: [createPersistedState()],
+  modules: {}
 });
